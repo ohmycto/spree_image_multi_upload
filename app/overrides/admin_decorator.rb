5374751ff412179de277b87c30c698fc2c23cc70
Maks Sleptsov <maksimator@gmail.com>
2013-03-07 09:36:59 +0200

    New image button replaced

--- app/overrides/admin_decorator.rb
+++ app/overrides/admin_decorator.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(
     :virtual_path => 'spree/admin/images/index',
-    :insert_before => %q{code[erb-silent]:contains(':page_actions')},
-    :text => %q{<% content_for :page_actions do %><li><%= link_to_with_icon('icon-upload', t('multi_upload.title'), multi_upload_admin_product_images_path(@product), :id => 'multi_upload_images_link', :class => 'button', :remote => true) %></li><% end %>},
+    :replace => %q{code[erb-loud]:contains('t(:new_image)')},
+    :text => %q{<%= link_to_with_icon('icon-upload', t('multi_upload.title'), multi_upload_admin_product_images_path(@product), :id => 'multi_upload_images_link', :class => 'button', :remote => true) %>},
     :name => 'add_images_multi_upload_button'
 )
